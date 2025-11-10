6740067e3117eaff118aedb75c1c7a5fe7fded13
Ferhat Ucar <fucar@firat.edu.tr>
2025-11-10 08:43:16 +0300

    10 Kasım Sınıf içi
    Ornekler Devam

---
+++ 03Selections/04-BMI_hesapVEtahmin.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ComputeAndInterpretBMI {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    // Prompt the user to enter weight in kilograms
+    System.out.print("Enter weight in kilograms: ");
+    double weightInKilograms = input.nextDouble();
+    
+    // Prompt the user to enter height in meters
+    System.out.print("Enter height in meters: ");
+    double heightInMeters = input.nextDouble();
+    
+    
+    // Compute BMI
+    double bmi = weightInKilograms / 
+      (heightInMeters * heightInMeters);
+
+    // Display result
+    System.out.println("BMI is " + bmi);
+    if (bmi < 18.5)
+      System.out.println("Underweight");
+    else if (bmi < 25)
+      System.out.println("Normal");
+    else if (bmi < 30)
+      System.out.println("Overweight");
+    else
+      System.out.println("Obese");
+  }
+}
